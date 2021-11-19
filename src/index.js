89efd7c10ac89142dd72fee2356a0eb938c858e7
Victoria Walker <vmsmith2014@gmail.com>
2021-11-19 08:39:34 -0500

    first commit - setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      {/* Base Layout wraps the next set of components/routes */}
+        <Routes>
+          <Route path="/" element={<App />} />
+          {/* All other routes here */}
+        </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
