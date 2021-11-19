9d4750a9015302321df469dcf5a799715eb81d5a
Victoria Walker <vmsmith2014@gmail.com>
2021-11-19 12:54:21 -0500

    issue detail basic layout; data fetched and parsed

--- src/components/IssueDetail.jsx
+++ src/components/IssueDetail.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import {useState, useEffect} from "react";
 import {useParams} from "react-router-dom";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm'
+// eslint-disable-next-line no-unused-vars
+import Card, {Body, Title, Text} from 'react-bootstrap/Card';
+import '../issue.css';
+
 
 function IssueDetail() {
     const {issue_number} = useParams();
     const [issue, setIssue] = useState([])
+    const markdown = issue.body;
     // const navigate = useNavigate();
 
     useEffect(() => {
@@ -18,17 +25,31 @@ function IssueDetail() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    // useEffect(() => {
-    //     console.log(issue);
-    // }, [issue]);
+    useEffect(() => {
+        console.log(issue);
+    }, [issue]);
 
     return (
         <div>
             <h2>{issue.title} #{issue_number}</h2>
             <p>{issue.user.login} opened this issue at {issue.created_at} • {issue.comments} comments </p>
+            <hr></hr>
 
-            <img src={issue.user.avatar_url} alt={`avatar for ${issue.user.login}`} />
+            <img className="avatar" src={issue.user.avatar_url} alt={`avatar for ${issue.user.login}`} />
             
+
+            <Card>
+            <Card.Body>
+                <Card.Link className="" href="">{issue.user.login}</Card.Link>
+                <span className="ms-2">commented at {issue.created_at}</span>
+                <hr />
+                <Card.Text>
+                    <ReactMarkdown children={markdown} remarkPlugins={[remarkGfm]} />
+                </Card.Text>
+                
+            </Card.Body>
+            </Card>
+
         </div>
     )
 }
